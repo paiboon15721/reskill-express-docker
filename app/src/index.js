3531e169a2221c767770dfb3675129b598c61b82
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 09:33:16 +0700

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ hello: 'world' })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
