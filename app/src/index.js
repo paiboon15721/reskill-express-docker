c480334dd1a93380287646ed11f7119aa109e4c4
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 09:42:06 +0700

    update

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan('combined'))
+app.use('/', express.static(`${__dirname}/public`))
 
 app.get('/', (req, res) => {
   res.send({ hello: 'world' })
