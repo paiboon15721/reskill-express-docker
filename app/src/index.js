fb2bf0bf5c691ff547cac8c1553666dc19e0329d
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 10:56:30 +0700

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const { initMongoose } = require('./init-mongoose')
+
+const { Employee } = mongoose.models
 
 const app = express()
 
@@ -10,7 +16,14 @@ app.get('/', (req, res) => {
   res.send({ hello: 'world' })
 })
 
+app.get('/employees', async (req, res) => {
+  const employees = await Employee.find()
+  res.send(employees)
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+initMongoose().then(() => {
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
 })
