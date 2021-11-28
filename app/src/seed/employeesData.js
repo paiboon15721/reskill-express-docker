ef569ceecb4c98bcda9155bea297fc73fe278d0b
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 10:25:52 +0700

    update

--- app/src/seed/employeesData.js
+++ app/src/seed/employeesData.js
@@ -3,13 +3,19 @@ const _ = require('lodash')
 
 const roles = ['Manager', 'Developer', 'SA', 'HR']
 
-const employee = {
+const createEmployee = () => ({
   name: faker.name.firstName(),
   lastName: faker.name.lastName(),
   email: faker.internet.email(),
   age: _.random(20, 50),
   salary: _.random(20000, 50000),
   role: roles[_.random(0, roles.length - 1)],
-}
+})
 
-console.log(employee)
+const emps = _.range(100).map(createEmployee)
+
+console.log(emps)
+// console.log(createEmployee())
+// console.log(createEmployee())
+// console.log(createEmployee())
+// console.log(createEmployee())
