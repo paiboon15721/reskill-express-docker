16f0ad90b810b554478488576d0ebdfdd56825b2
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 13:15:51 +0700

    update

--- app/src/components/auth/controller.js
+++ app/src/components/auth/controller.js
@@ -1,5 +1,14 @@
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+// bcrypt.hash('1234', 10).then(hash => {
+//   console.log(hash)
+
+//   bcrypt.compare('12345', hash).then(res => {
+//     console.log('result', res)
+//   })
+// })
 
 module.exports = app => {
   const { User } = mongoose.models
@@ -11,7 +20,8 @@ module.exports = app => {
       res.status(400).send({ message: 'email is in use' })
       return
     }
-    const user = await User.create(body)
+    const hash = await bcrypt.hash(body.password, 10)
+    const user = await User.create({ ...body, password: hash })
     res.send(user)
   })
 }
