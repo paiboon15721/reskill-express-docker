edabdd1039929b88b093cd66746b236bedb0e392
Paiboon Auengkongkatong <paiboon.a@thoughtworks.com>
2021-11-28 13:48:37 +0700

    update

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const swaggerUi = require('swagger-ui-express')
 const controllers = require('./components')
 
 const { initMongoose } = require('./init-mongoose')
 
 const app = express()
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null))
 app.use(morgan('combined'))
 app.use('/', express.static(`${__dirname}/public`))
 
